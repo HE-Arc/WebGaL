915c38fdb493726dfc7b32a2ecaecd0331397957
Karim Luy <karim.luy@he-arc.ch>
2017-03-13 13:25:45 +0100

    closing issue #1

--- project/WebGal/WebGal/wsgi.py
+++ project/WebGal/WebGal/wsgi.py
@@ -2,15 +2,17 @@
 WSGI config for WebGal project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/1.10/howto/deployment/wsgi/
 """
-
 import os
+from os.path import abspath, dirname
+from sys import path
 
-from django.core.wsgi import get_wsgi_application
+SITE_ROOT = dirname(dirname(abspath(__file__)))
+path.append(SITE_ROOT)
+
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "WebGal.settings")
 
+
+from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
