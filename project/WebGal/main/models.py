e7952565921a940f1db62fc78cd174eca57e7423
Karim Luy <karim.luy@he-arc.ch>
2017-03-19 15:45:31 +0100

    created models, modified base.py for migrations

--- project/WebGal/main/models.py
+++ project/WebGal/main/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+
+class Project(models.Model):
+    project_name = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+    likes = models.IntegerField(default=0)
+    shares = models.IntegerField(default=0)
+
+
+class User(models.Model):
+    username = models.CharField(max_length=60)
+    password = models.CharField(max_length=30)
+    projects = models.IntegerField(default=0)
+    email = models.EmailField()
+
+
+class Comment(models.Model):
+    pub_date = models.DateTimeField('date published')
+    text = models.CharField(max_length=1000)
+    project = models.ForeignKey(Project, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
