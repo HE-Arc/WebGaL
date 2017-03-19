8238cc34ad7f92ed18ef3cbbbe84fe672257cea4
Karim Luy <karim.luy@he-arc.ch>
2017-03-19 16:47:30 +0100

    corrected some indentation error

--- project/WebGal/main/urls.py
+++ project/WebGal/main/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-	url(r'^project', views.project, name='project'),
-	url(r'^login', views.login, name='login'),
-	url(r'^upload', views.upload, name='upload'),
+    url(r'^project', views.project, name='project'),
+    url(r'^login', views.login, name='login'),
+    url(r'^upload', views.upload, name='upload'),
 ]
