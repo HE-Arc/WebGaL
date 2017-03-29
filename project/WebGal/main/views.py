a61f27d6b7e8aded9396bd65b58f5ffb8d535553
Karim Luy <karim.luy@he-arc.ch>
2017-03-29 13:54:58 +0200

    initiated routing to upload page, now to form it all up!

--- project/WebGal/main/views.py
+++ project/WebGal/main/views.py
@@ -1,17 +1,20 @@
 from django.http import HttpResponse
 from django.template import loader
 
+
 def index(request):
     template = loader.get_template('index.html')
     return HttpResponse(template.render())
 
-	
+
 def project(request):
     return HttpResponse("You're viewing a project.")
-	
+
+
 def login(request):
     return HttpResponse("You can login here.")
-	
+
+
 def upload(request):
-    return HttpResponse("You can upload a project here.")
-	
+    template = loader.get_template('upload.html')
+    return HttpResponse(template.render())
