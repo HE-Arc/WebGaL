c10a00af22662900cb54b71f75d5a0917164c2c4
Fabio Manuel da Mota Marques <fabiomanuel.damotamarques@he-arc.ch>
2017-04-18 15:29:17 +0200

    Set projects likes and shares to positiveintegerfield instead of integerfield

--- project/WebGal/main/models.py
+++ project/WebGal/main/models.py
@@ -9,8 +9,8 @@ def upload_to(instance, filename):
 class Project(models.Model):
     project_name = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
-    likes = models.IntegerField(default=0)
-    shares = models.IntegerField(default=0)
+    likes = models.PositiveIntegerField(default=0)
+    shares = models.PositiveIntegerField(default=0)
     description = models.TextField(max_length=4000)
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, null=True)
     image = models.ImageField(upload_to=upload_to)
