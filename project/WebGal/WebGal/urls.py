0faa2edaae6f66ddee203e7304f624d643a48cf9
Yoan Blanc <yoan@dosimple.ch>
2017-04-23 11:52:31 +0200

    Version testée
    (voir FIXME.md)
    
    Signed-off-by: Yoan Blanc <yoan@dosimple.ch>

--- project/WebGal/WebGal/urls.py
+++ project/WebGal/WebGal/urls.py
@@ -1,4 +1,5 @@
-from django.conf.urls import url, include
+from django.conf import settings
+from django.conf.urls import url, include, static
 from django.contrib import admin
 
 urlpatterns = [
@@ -7,3 +8,15 @@ urlpatterns = [
     url('^', include('django.contrib.auth.urls')),
     url('^', include('registration.backends.hmac.urls')),
 ]
+
+if settings.DEBUG:
+    try:
+        import debug_toolbar
+        urlpatterns += [
+            url(r'^__debug__/', include(debug_toolbar.urls)),
+        ]
+    except ImportError:
+        pass
+
+    urlpatterns += static.static(
+        settings.STATIC_URL, document_root=settings.STATIC_ROOT)
